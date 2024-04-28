f9db6e19b2d653a05987091f5108353b2ee0b8f8
Zelenskiyas <zelensk1973@gmail.com>
2024-04-28 12:35:36 +0300

    feat: add post animation

--- src/components/PostList.jsx
+++ src/components/PostList.jsx
@@ -1,27 +1,33 @@
-import React from 'react';
-import PostItem from './PostItem';
+import React from "react";
+import PostItem from "./PostItem";
+import {CSSTransition, TransitionGroup} from "react-transition-group";
 
-
-const PostList = ({posts, title, remove}) => {
-
+const PostList = ({ posts, title, remove }) => {
   if (!posts.length) {
-    return (
-      <h1  style={{textAlign: 'center'}}>
-        Posts not found
-      </h1>
-    )
+    return <h1 style={{ textAlign: "center" }}>Posts not found</h1>;
   }
 
   return (
-    <div className='posts'>
-         <h1 style={{textAlign: 'center'}}>{title}</h1>
-      {posts.map((post, index) =>
-        <PostItem remove={remove} number={index+1}  post={post} key={post.id} />          
-      )}
+    <div className="posts">
+      <h1 style={{ textAlign: "center" }}>{title}</h1>
+      <TransitionGroup>
+        {posts.map((post, index) => 
+          <CSSTransition 
+            key={post.id}
+            timeout={500}
+            classNames="post"
+          >
+          <PostItem
+            remove={remove}
+            number={index + 1}
+            post={post}
+          />        
+        </CSSTransition>
+        )}
+      </TransitionGroup>
       
-      </div>
-  )
+    </div>
+  );
+};
 
-}
-
-export default PostList
+export default PostList;
